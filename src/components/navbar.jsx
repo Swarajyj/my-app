febbcfb72ce523ba333faa62be69f61813edb1f5
swarajyj <yjswaraj@gmail.com>
2021-12-17 23:21:58 +0530

    stateless functions

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,34 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav class="navbar navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-          Navbar
-        </a>
-      </nav>
-    );
-  }
-}
+// class NavBar extends Component {
+//   render() {
+//     return (
+//       <nav className="navbar navbar-light bg-light">
+//         <a classname="navbar-brand" href="#">
+//           Navbar{" "}
+//           <span className="badge badge-pill badge-secondary">
+//             {this.props.totalCounters}
+//           </span>
+//         </a>
+//       </nav>
+//     );
+//   }
+// }
+
+// export default NavBar;
+
+//Stateless functional Component
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a classname="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
